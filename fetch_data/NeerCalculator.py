Fixing a normalization bug in weight

fetch_data/NeerCalculator.py
@@ -2,6 +2,9 @@ from dotenv import load_dotenv
 import os
 import pandas as pd
 import numpy as np
+from scipy.interpolate import interp1d
+from dateutil import relativedelta as dt
+
 from utils.utils import *
 from utils.config import *
 
@@ -23,13 +26,22 @@ class NeerCalculator:
         self.neerWeightDf["reporter"] = self.neerWeightDf["reporter"].map(country2Ccy).fillna(self.neerWeightDf["reporter"])
         self.neerWeightDf["partner"] = self.neerWeightDf["partner"].map(country2Ccy).fillna(self.neerWeightDf["partner"])
         weightLag = parseTimeDelta(os.getenv("WEIGHT_LAG"))
+        # use weight up to previous year due to time lag of trade data update
         self.neerWeightDf.index=pd.to_datetime(self.neerWeightDf.index).map(lambda x: x + weightLag)
+        # replace NaN weight with 0
+        # self.neerWeightDf = self.neerWeightDf.fillna(0.0)
+        # flat extrapolation to 1Y
+        lastDate = self.neerWeightDf.index[-1]
+        dfLastDate = self.neerWeightDf.loc[lastDate].copy()
+        lastDate += dt(years=1)
+        dfLastDate.index = [lastDate] * len(dfLastDate)
+        self.neerWeightDf = pd.concat([self.neerWeightDf, dfLastDate])
 
         # further, interpolate weight
         dates = list(self.fxRateDataDf.index)
         dates_num = [date.timestamp() for date in dates]
-        neerWeightDfTimestamp = neerWeightDf.copy()
-        neerWeightDfTimestamp.index = [t.timestamp() for t in neerWeightDf.index]
+        neerWeightDfTimestamp = self.neerWeightDf.copy()
+        neerWeightDfTimestamp.index = [t.timestamp() for t in neerWeightDfTimestamp.index]
         neerWeightDfInterps = []
         for reporter in neerWeightDfTimestamp["reporter"].unique():
             for partner in neerWeightDfTimestamp["partner"].unique():
@@ -48,7 +60,10 @@ class NeerCalculator:
                     #     neerWeightDfInterps.append(neerWeightDfInterpDict)
 
                     # vectorize
-                    weights = np.interp(dates_num, ts, ws)
+                    # weights = np.interp(dates_num, ts, ws)
+                    # interpolator = interp1d(ts, ws, bounds_error=False, fill_value=np.nan)
+                    interpolator = interp1d(ts, ws, bounds_error=False, fill_value=0.0)
+                    weights = interpolator(dates_num)
                     # neerWeightDfInterpDict = {
                     #     "Date": dates,
                     #     "reporter": reporter,
@@ -97,8 +112,11 @@ class NeerCalculator:
             # vectorize
             weights_report_ccy = neerWeightDfInterpPivot[(neerWeightDfInterpPivot["reporter"] == report_ccy)]
             weight_report_ccy_ordered = weights_report_ccy[self.fxRateDataDf.columns]
-            wln = (weight_report_ccy_ordered * lnFx).sum(axis=1, skipna=True)
-            ln_NEER_df_ccy = (wln - lnFx[report_ccy]).to_frame()
+            # wln = (weight_report_ccy_ordered * lnFx).sum(axis=1, skipna=True)
+            # ln_NEER_df_ccy = (wln - lnFx[report_ccy]).to_frame()
+            lnFxMinuslnReportCcy = lnFx.subtract(lnFx[report_ccy], axis=0)
+            wln = (weight_report_ccy_ordered * lnFxMinuslnReportCcy).sum(axis=1, skipna=True)
+            ln_NEER_df_ccy = wln.to_frame()
             NEER_df_ccy = np.exp(ln_NEER_df_ccy) * 100
             NEER_df_ccy.columns = [report_ccy]
             NEER_dfs.append(NEER_df_ccy)

fetch_data/NeerWeight.py
@@ -37,11 +37,17 @@ class ImfNeerWeight(NeerWeight):
             df_trade = df_trade.reset_index()
             df_trade["import_export"] = df_trade["XG_FOB_USD"] + df_trade["MG_CIF_USD"]
             df_trade = df_trade[["Date", "reporter", "partner", "import_export"]]
+            # if import_export is nan, use a mirror value ("reporter" and "partner" are swapped)
+            df_mirror = df_trade.rename(columns={"reporter": "partner", "partner": "reporter", "import_export": "import_export_mirror"})
+            df_trade = df_trade.merge(df_mirror, on=["Date", "reporter", "partner"], how="left")
+            df_trade["import_export"] = df_trade["import_export"].fillna(df_trade["import_export_mirror"])
+            df_trade = df_trade[["Date", "reporter", "partner", "import_export"]]
             # ratio among all parter countries
             df_trade["weight"] = df_trade["import_export"] / df_trade.groupby(["Date", "reporter"])["import_export"].transform("sum")
             # if weight < minWeightCutOff, impute it as 0, then renormalize
             df_trade["weight"] = np.where(df_trade["weight"] < self.minWeightCutoff, 0.0, df_trade["weight"])
             df_trade["weight"] = df_trade["weight"] / df_trade.groupby(["Date", "reporter"])["weight"].transform("sum")
+            # sum_one = df_trade.groupby(["Date", "reporter"])["weight"].transform("sum")
             df_trade = df_trade[["Date", "reporter", "partner", "weight"]]
             df_trade = pd.pivot_table(
                 df_trade,
@@ -49,14 +55,24 @@ class ImfNeerWeight(NeerWeight):
                 columns="partner",
                 values="weight")
             df_trade = df_trade.reset_index().sort_values(["reporter", "Date"])
-            partner_cols = df_trade.columns.difference(["reporter", "Date"])
+            partner_cols = df_trade.columns.difference(["reporter", "Date"]) # extract columns (currencies) other than "reporter", "Date"
+            freq_factor = 0
+            if self.freq == "A":
+                freq_factor = 1
+            elif self.freq == "M":
+                freq_factor = 12
+            else:
+                raise ValueError("Trade data only supports frequency of annually or monthly, but frequency is: " + self.freq)
             df_trade[partner_cols] = df_trade.groupby("reporter")[partner_cols].transform(
-                lambda x: x.rolling(window=3).mean()
+                lambda x: x.rolling(window=3*freq_factor, min_periods=1).mean()
             )
             df_trade = pd.melt(df_trade,
                                id_vars=["Date", "reporter"],
                                var_name="partner",
                                value_name="weight")
+            # because the moving average of partially missing value distorts normalization,
+            # renormalizatoin is required
+            df_trade["weight"] = df_trade["weight"] / df_trade.groupby(["Date", "reporter"])["weight"].transform("sum")
             df_trade = df_trade.dropna().reset_index()
             df_trade = df_trade.set_index("Date")
             df_trade = df_trade[["reporter", "partner", "weight"]]