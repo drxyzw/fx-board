1cf981d0562757af5eeb5353eb3951ad512bddd0
drxyzw <hirotoo20@gmail.com>
2026-02-23 12:24:02 +0800

    update

--- src/app.js
+++ src/app.js
@@ -3,9 +3,14 @@ let timeseriesData;
 let dates;
 
 async function loadTimeSeries() {
-    const res= await fetch("data/NEER_CHART_DAILY.json");
+    const res= await fetch("/data/NEER_CHART_DAILY.json");
     timeseriesData = await res.json();
-    dates = timeseriesData.dates;
+    timeseriesData = {
+        "Date": timeseriesData.Date,
+        "USD": timeseriesData.USD,
+        "JPY": timeseriesData.JPY,
+        "EUR": timeseriesData.EUR}
+    dates = timeseriesData.Date;
 }
 
 function plotTimeSeries() {
@@ -26,75 +31,110 @@ function plotTimeSeries() {
 async function loadReporter(reporter) {
     if(reporterCache[reporter]) return reporterCache[reporter]
 
-    const res = await fetch("data/${reporter}.json");
+    const res = await fetch(`/data/detail/${reporter}.json`);
     const data = await res.json();
     reporterCache[reporter] = data;
     return data;
 }
 
-function getDateIndex(date) {
-    return dates.indexOf(date);
+function getDateIndex(date, datesArray) {
+    return datesArray.indexOf(date);
 }
 
 function computeRangeValues(data, startIdx, endIdx) {
     const result = {};
-    for (const partner in data.partners) {
-        const arr = data.partners[partner];
-        const endVal = arr[endIdx];
-        const startVal = startIdx > 0 ? arr[startIdx] : 0;
-        result[partner] = endVal - startVal
+    for (const partner in data.partner) {
+        const arr = data.partner[partner];
+        result[partner] = {}
+        for(const key of Object.keys(arr)) {
+            const endVal = arr[key][endIdx];
+            const startVal = startIdx > 0 ? arr[key][startIdx] : 0;
+            result[partner][key] = endVal - startVal
+        }
+        result[partner]["cum_weight"] /= endIdx - startIdx
     }
     return result;
 }
 
 function plotHeatmap(reporter, values) {
-    const partners = Objects.keys(values);
-    const z = [partners.map(p => values[p])];
+    const partners = Object.keys(values);
+    const ret = partners.map(p => values[p]["cum_return"]);
+    const weight = partners.map(p => values[p]["cum_weight"]);
+    const contrib = partners.map(p => values[p]["cum_contribution"]);
+    const group = Array(ret.length).fill("");
 
-    Plotly.newPlot("heatmap", [{
-        z: z,
-        x: partners,
-        y: [reporter],
-        type: "heatmap",
-        colorscale: "RdBu",
-        reversescale: true,
-    }], {
-        title: `Return vs ${reporter}`
-    });
+    const data = [{
+        type: "treemap",
+        labels: partners,
+        parents: group,
+        values: weight,
+        marker: {
+            colors: contrib,
+            colorscale: [
+                [0.0, "#8b0000"],
+                [0.5, "#f0f0f0"],
+                [1.0, "#006400"]
+            ],
+            cmin: -1.0,
+            cmax: 1.0,
+            cmid: 0.0,
+            line: {width: 1}
+        },
+        textinfo: "label",
+        hovertemplate:
+            "<b>%{label}</b><br>" +
+            "Weight: %{value.2%}<br>" +
+            "Contribution: %{color.2f}%<extra></extra>"
+    }]
+    const layout = {
+        margin: {t:10, l:0, r:0, b:0},
+    };
+    Plotly.newPlot("heatmap", data, layout);
+    // Plotly.newPlot("heatmap", [{
+    //     z: z,
+    //     x: partners,
+    //     y: [reporter],
+    //     type: "heatmap",
+    //     colorscale: "RdBu",
+    //     reversescale: true,
+    // }], {
+    //     title: `Return vs ${reporter}`
+    // });
 }
 
 function plotMap(values) {
-    const partners = Object.keys(value);
+    const partners = Object.keys(values);
     const z = partners.map(p => values[p]);
 
-    Plotly.newPost("map", [{
+    Plotly.newPlot("map", [{
         type: "choropleth",
         locations: partners,
         z: z,
-        loctionmode: "ISO-3",
+        locationmode: "ISO-3",
         colorscale: "RdBu",
         reversescale: true,
     }], {
-        geo: { projection: {type: "natual earth"}},
+        geo: { projection: {type: "natural earth"}},
         title: "Returns by Trade Partner",
     });
 }
 
-document.addEventListener("DOMcontentLoaded", async() => {
+document.addEventListener("DOMContentLoaded", async() => {
     await loadTimeSeries();
     plotTimeSeries();
     
-    document.getElementById("timeseries").onabort("plotly_hover", async function(event) {
+    document.getElementById("timeseries").on("plotly_hover", async function(event) {
         const reporter = event.points[0].data.name;
         const start = document.getElementById("startDate").value
         const end = document.getElementById("endDate").value
-
         if(!start | !end) return;
 
-        const startIdx = getDateIndex(startDate);
-        const endIdx = getDateIndex(endDate);
         const reporterData = await loadReporter(reporter);
-        const values = computeRangeValues(reporterData, startIdx, endIdx);
+        const startIdx = getDateIndex(start, reporterData.Date);
+        const endIdx = getDateIndex(end, reporterData.Date);
+        if(startIdx == -1 | endIdx == -1) return;
+
+        const values = endIdx > startIdx ? computeRangeValues(reporterData, startIdx, endIdx) : None;
         plotHeatmap(reporter, values);
         plotMap(values);
     });
