1c16299f10aa5ef6f6d283bfb911ec64011bfe25
drxyzw <hirotoo20@gmail.com>
2026-03-03 13:25:23 +0800

    files good both for vite and github pages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+    base: '/fx-board/', // IMPORTANT: Match your GitHub repository name exactly
     root: ".",              // frontend folder
     publicDir: "public",    // default
     server: {
